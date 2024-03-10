0762f3dd63d60ad04c73cdcce1f7ac5b0a2cfedb
Fergus Findley <fergus.findley@gmail.com>
2024-03-10 17:58:02 +0100

    finish accessability

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,13 +6,13 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 
-const FormSchemaOld = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
-    date: z.string(),
-});
+// const FormSchemaOld = z.object({
+//     id: z.string(),
+//     customerId: z.string(),
+//     amount: z.coerce.number(),
+//     status: z.enum(['pending', 'paid']),
+//     date: z.string(),
+// });
 
 const FormSchema = z.object({
     id: z.string(),
@@ -92,24 +92,35 @@ export async function createInvoice(prevState: State, formData: FormData) {
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+    id: string,
+    prevState: State,
+    formData: FormData,
+) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
+
     try {
         await sql`
-            UPDATE invoices
-            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-            WHERE id = ${id}
-            `;
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
     } catch (error) {
-        return {
-            message: 'Database Error: Failed to Update Invoice.',
-        };
+        return { message: 'Database Error: Failed to Update Invoice.' };
     }
 
     revalidatePath('/dashboard/invoices');
